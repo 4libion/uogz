51b22c88abf025df9a862bd5d95379522479c194
alibi <tolebayalibi24@gmail.com>
2021-11-20 20:03:46 +0600

    commit

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
     $connect = mysqli_connect(
-        'localhost',
-        'root',
-        '',
-        'uogz'
+        'us-cdbr-east-04.cleardb.com',
+        'bbfa49c7e13d07',
+        'b31b48ff',
+        'heroku_e545b6a41bed266'
     );
     if (!$connect) {
         die('Error occured while connecting to database');
